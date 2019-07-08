d0944efafc767c2debaee0ee9f645125c2eb0f0f
Ben Ling <belinghy@gmail.com>
2019-07-08 11:30:37 -0700

    Fix path in compcan scripts

--- computecanada/cedar_run_playground_train.sh
+++ computecanada/cedar_run_playground_train.sh
@@ -5,6 +5,6 @@
 #SBATCH --exclusive
 #SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=400M
-cd /home/belinghy/projects/def-vandepan/belinghy/rl-experiments/playground
+cd /home/belinghy/projects/def-vandepan/belinghy/SteppingStone/playground
 . /home/belinghy/projects/def-vandepan/belinghy/research-env/bin/activate
 python train.py
--- computecanada/graham_run_playground_train.sh
+++ computecanada/graham_run_playground_train.sh
@@ -5,6 +5,6 @@
 #SBATCH --exclusive
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=400M
-cd /home/belinghy/projects/def-vandepan/belinghy/rl-experiments/playground
+cd /home/belinghy/projects/def-vandepan/belinghy/SteppingStone/playground
 . /home/belinghy/projects/def-vandepan/belinghy/research-env/bin/activate
 python train.py
