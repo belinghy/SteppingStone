909991fc2bff191a3869c760d70e78818029061e
Zhaoming Xie <zxieaa@gmail.com>
2019-12-22 11:38:43 -0800

    ensemble value

--- algorithms/ppo.py
+++ algorithms/ppo.py
@@ -17,6 +17,7 @@ class PPO(object):
         max_grad_norm=None,
         use_clipped_value_loss=True,
         mirror_function=None,
+        num_ensembles = 1
     ):
         self.actor_critic = actor_critic
 
@@ -33,6 +34,8 @@ class PPO(object):
         self.mirror_function = mirror_function
 
         self.optimizer = optim.Adam(actor_critic.parameters(), lr=lr, eps=eps)
+
+        self.value_optimizer = optim.Adam(actor_critic.parameters(), lr=10*lr, eps=eps)
 
     def update(self, rollouts):
         advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
@@ -103,3 +106,56 @@ class PPO(object):
         dist_entropy_epoch /= num_updates
 
         return value_loss_epoch, action_loss_epoch, dist_entropy_epoch
+
+
+    def update_values(self, rollouts):
+        advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
+        advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
+        value_loss_epoch = 0
+
+        for e in range(self.ppo_epoch):
+            data_generator = rollouts.feed_forward_generator(
+                advantages, self.num_mini_batch
+            )
+
+            for sample in data_generator:
+                if self.mirror_function is not None:
+                    observations_batch, states_batch, actions_batch, value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, adv_targ = self.mirror_function(
+                        sample
+                    )
+                else:
+                    observations_batch, states_batch, actions_batch, value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, adv_targ = (
+                        sample
+                    )
+
+                values, action_log_probs, dist_entropy, states = self.actor_critic.evaluate_actions(
+                    observations_batch, states_batch, masks_batch, actions_batch
+                )
+
+                if self.use_clipped_value_loss:
+                    value_pred_clipped = value_preds_batch + (
+                        values - value_preds_batch
+                    ).clamp(-self.clip_param, self.clip_param)
+                    value_losses = (values - return_batch).pow(2)
+                    value_losses_clipped = (value_pred_clipped - return_batch).pow(2)
+                    value_loss = (
+                        0.5 * torch.max(value_losses, value_losses_clipped).mean()
+                    )
+                else:
+                    value_loss = 0.5 * (return_batch - values).pow(2).mean()
+
+                self.value_optimizer.zero_grad()
+                (
+                    value_loss * self.value_loss_coef
+                ).backward()
+                # nn.utils.clip_grad_norm_(
+                #     self.actor_critic.parameters(), self.max_grad_norm
+                # )
+                self.optimizer.step()
+
+                value_loss_epoch += value_loss.item()
+                #print(value_loss)
+        num_updates = self.ppo_epoch * self.num_mini_batch
+
+        value_loss_epoch /= num_updates
+        return value_loss_epoch
